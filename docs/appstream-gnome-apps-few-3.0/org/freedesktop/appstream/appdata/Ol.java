96a839b6d6d6a4762daad2909c9d9cf2a52dd3be
Jorge García <jgarciao@gmail.com>
2017-04-21 23:03:06 +0200

    Generate new clients using appstream-gnome-apps-few-3.0.xsd

--- docs/appstream-gnome-apps-few-3.0/org/freedesktop/appstream/appdata/Ol.java
+++ docs/appstream-gnome-apps-few-3.0/org/freedesktop/appstream/appdata/Ol.java
@@ -8,10 +8,14 @@
 
 package org.freedesktop.appstream.appdata;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlMixed;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
@@ -26,7 +30,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element ref="{}lang" maxOccurs="unbounded" minOccurs="0"/>
+ *         &lt;element ref="{}li" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -37,40 +41,43 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "lang"
+    "content"
 })
-@XmlRootElement(name = "languages")
-public class Languages {
+@XmlRootElement(name = "ol")
+public class Ol {
 
-    protected List<Lang> lang;
+    @XmlElementRef(name = "li", type = JAXBElement.class, required = false)
+    @XmlMixed
+    protected List<Serializable> content;
 
     /**
-     * Gets the value of the lang property.
+     * Gets the value of the content property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the lang property.
+     * This is why there is not a <CODE>set</CODE> method for the content property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getLang().add(newItem);
+     *    getContent().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Lang }
+     * {@link String }
+     * {@link JAXBElement }{@code <}{@link String }{@code >}
      * 
      * 
      */
-    public List<Lang> getLang() {
-        if (lang == null) {
-            lang = new ArrayList<Lang>();
+    public List<Serializable> getContent() {
+        if (content == null) {
+            content = new ArrayList<Serializable>();
         }
-        return this.lang;
+        return this.content;
     }
 
 }
